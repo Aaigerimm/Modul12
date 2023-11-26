ca04f429f34867a1723de4ea63818dcc01205bee
nurik0708 <aaikossya09@gmail.com>
2023-11-26 10:00:57 +0600

    Добавьте файлы проекта.

---
+++ Modul12/MyClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul12
+{
+    public class MyClass : IPropertyChanged
+    {
+        private int myProperty;
+
+        public int MyProperty
+        {
+            get => myProperty;
+            set
+            {
+                if (myProperty != value)
+                {
+                    myProperty = value;
+                    OnMyPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyEventHandler PropertyChanged;
+
+        protected virtual void OnMyPropertyChanged()
+        {
+            PropertyChanged?.Invoke(this, new PropertyEventArgs(nameof(MyProperty)));
+        }
+    }
+
+}
---
+++ Modul12/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul12
+{
+    public delegate void PropertyEventHandler(object sender, PropertyEventArgs e);
+
+    public class PropertyEventArgs : EventArgs
+    {
+        public string PropertyName { get; }
+
+        public PropertyEventArgs(string propertyName)
+        {
+            PropertyName = propertyName;
+        }
+    }
+    public interface IPropertyChanged
+    {
+        event PropertyEventHandler PropertyChanged;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myObject = new MyClass();
+            myObject.PropertyChanged += MyObject_PropertyChanged;
+
+            Console.WriteLine("Setting MyProperty to 42");
+            myObject.MyProperty = 42;
+
+            Console.WriteLine("\nSetting MyProperty to 42 again (no change event should be fired)");
+            myObject.MyProperty = 42;
+
+            Console.ReadLine();
+        }
+        private static void MyObject_PropertyChanged(object sender, PropertyEventArgs e)
+        {
+            Console.WriteLine($"Property '{e.PropertyName}' changed in the object {sender}");
+        }
+    }
+}
